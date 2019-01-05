20f8a9b4a11e42f6f589bdf5cb65be4c2a3698c1
cyangyu <cyangyu163@163.com>
2019-01-05 21:03:51 +0800

    ...

--- algorithm/maximum_subarray.cpp
+++ algorithm/maximum_subarray.cpp
@@ -5,6 +5,28 @@ USESTD
 class Solution {
 public:
     int maxSubArray(vector<int> &nums) {
-        
+        int size = nums.size();*/
+        vector<int> dp(size);
+
+        int sum = 0, maxSum = 0;
+
+        for (int i = 0; i < size; i++) 
+            dp[i] = max(nums[i], nums[i] + dp[ i - 1]);
+
+        maxSum = dp[0];
+        for (int j = 0; j < size; j++)
+            if (dp[i] > maxSum)
+                maxSum = dp[i];
+
+        return maxSum;
     }
 };
+
+int main(int argc, char *argv[])
+{
+    vector<int> v = { -2, 1 };
+
+    Solution solution;
+    cout << solution.maxSubArray(v) << endl;
+    return 0;
+}
